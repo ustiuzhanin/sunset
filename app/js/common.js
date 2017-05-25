98871ebbe83e8d310367baf768c45f556f81b78a
Slava <ucheba00@gmail.com>
2017-05-25 02:05:29 -0600

    add sliding menu(jquery), adaptivity

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,28 @@
-$(function() {
+$(document).ready(function() {
 
-	// Custom JS
+	$('.mobile-menu-button').click(function() {
+		$(this).fadeOut('50', function() {
+			$('.mobile-menu-close').fadeIn('200');
+		});
+		$('.mobile-nav').animate({
+			right: '0px'
+		}, 200);
+		$('body').animate({
+			right: '230px'
+		}, 200);
+	});
+
+	$('.mobile-menu-close').click(function() {
+		$(this).fadeOut('50', function() {
+			$('.mobile-menu-button').fadeIn('200');
+		});
+		$('.mobile-nav').animate({
+			right: '-230px'
+		}, 200);
+		$('body').animate({
+			right: '0px'
+		}, 200);
+	});
+
 
 });
