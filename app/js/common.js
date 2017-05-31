b3ce8762ef96aac4c2b8c5c78cc980c926008881
Slava <ucheba00@gmail.com>
2017-05-31 16:12:20 -0600

    finish header

--- app/js/common.js
+++ app/js/common.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
 
 	$('.mobile-menu-button').click(function() {
 		$(this).fadeOut('50', function() {
-			$('.mobile-menu-close').fadeIn('200');
+			$('.mobile-menu-close').fadeIn('0');
 		});
+		$('.mobile-menu-close .fa').animate({
+			right: '245px'
+		}, 200);
 		$('.mobile-nav').animate({
 			right: '0px'
 		}, 200);
@@ -16,6 +19,9 @@ $(document).ready(function() {
 		$(this).fadeOut('50', function() {
 			$('.mobile-menu-button').fadeIn('200');
 		});
+		$('.mobile-menu-close .fa').animate({
+			right: '10px'
+		}, 200);
 		$('.mobile-nav').animate({
 			right: '-230px'
 		}, 200);
